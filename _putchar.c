0a101dfe8b10db7762eb46958eed814210088e8f
Marouane Ed-Dahraoui <eddahraouimarouane@gmail.com>
2023-07-23 08:50:30 -0700

    add _putchar and puts to print character

---
+++ _putchar.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[BUFF_SIZE];
+
+	if (c == BUFF_FLUSH || i >= BUFF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUFF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return: void
+ */
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
